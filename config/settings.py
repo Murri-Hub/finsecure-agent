6c41880ffbf60e4e3f9176a611ed92fa2c8f9544
Murri-Hub <stefano.murrighile@gmail.com>
2026-02-21 09:17:05 +0100

    Create settings.py

---
+++ config/settings.py
@@ -0,0 +1,10 @@
+import os
+
+BASE_DIR = os.getenv(
+    "FINSECURE_BASE_DIR",
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+)
+
+RAW_DATA_DIR = os.path.join(BASE_DIR, "data", "raw")
+PROCESSED_DIR = os.path.join(BASE_DIR, "data", "processed")
+OUTPUT_DIR = os.path.join(BASE_DIR, "outputs")
